6f7e4460dba45d3f55cbbbaccaa5a34dcae771c5
Madhav Khosla <madhav@Madhavs-MacBook-Pro.local>
2017-07-05 23:44:41 -0400

    fixup

--- auditbot.go
+++ auditbot.go
@@ -83,79 +83,80 @@ func main() {
 
 Loop:
 	for {
-		select {
-		case msg := <-rtm.IncomingEvents:
-			fmt.Println("Event Received: ")
-			switch ev := msg.Data.(type) {
+		msg := <-rtm.IncomingEvents
+		fmt.Println("Event Received: ")
+		switch ev := msg.Data.(type) {
 
-			case *slack.ConnectedEvent:
-				fmt.Println("Connection counter:", ev.ConnectionCount)
+		case *slack.ConnectedEvent:
+			fmt.Println("Connection counter:", ev.ConnectionCount)
 
-			case *slack.MessageEvent:
-				formBotClient := FormBotClient{rtm, ev}
-				fmt.Printf("Message: %v\n", ev.Msg.Text)
-				info := rtm.GetInfo()
-				prefix := fmt.Sprintf("<@%s>", info.User.ID)
+		case *slack.MessageEvent:
+			formBotClient := FormBotClient{rtm, ev}
+			fmt.Printf("Message: %v\n", ev.Msg.Text)
+			info := rtm.GetInfo()
+			prefix := fmt.Sprintf("<@%s>", info.User.ID)
 
-				// Form bot help commands
-				if ev.User != info.User.ID && (ev.Text == prefix || ev.Text == fmt.Sprintf("%s help", prefix)) {
-					postMessgeParameters := slack.NewPostMessageParameters()
-					postMessgeParameters.Attachments = []slack.Attachment{
-						{
-							Title: "Command to start new intake form",
-							Text:  "@formbot create [EID]",
-							Color: "#7CD197",
-						},
-					}
-					rtm.PostMessage(ev.Channel, fmt.Sprintf("Formbot help commands"), postMessgeParameters)
+			// Form bot help commands
+			if ev.User != info.User.ID && (ev.Text == prefix || ev.Text == fmt.Sprintf("%s help", prefix)) {
+				postMessgeParameters := slack.NewPostMessageParameters()
+				postMessgeParameters.Attachments = []slack.Attachment{
+					{
+						Title: "Command to start new intake form",
+						Text:  "@formbot create [EID]",
+						Color: "#7CD197",
+					},
 				}
+				rtm.PostMessage(ev.Channel, fmt.Sprintf("Formbot help commands"), postMessgeParameters)
+			}
 
-				// Form bot start commands
-				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, fmt.Sprintf("%s create", prefix)) {
-					inputStringLength := strings.Split(ev.Text, " ")
+			// Form bot start commands
+			if ev.User != info.User.ID && strings.HasPrefix(ev.Text, fmt.Sprintf("%s create", prefix)) {
+				inputStringLength := strings.Split(ev.Text, " ")
 
-					// Input command not correct
-					if len(inputStringLength) != 3 {
-						rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("Invalid input command"), ev.Channel))
-					} else {
-						Eid = inputStringLength[2]
-						if _, err := os.Stat(fmt.Sprintf("/Users/madhav/%s", inputStringLength[2])); err != nil {
-							if os.IsNotExist(err) {
-								// file does not exist
-								f, err := os.Create(fmt.Sprintf("/Users/madhav/%s", inputStringLength[2]))
-								if err != nil {
-									fmt.Errorf("ERROR in creating a file \n")
-								}
-								w = bufio.NewWriter(f)
-								go formBotClient.sendQuestions(c)
-							} else {
-								// other error
+				// Input command not correct
+				if len(inputStringLength) != 3 {
+					rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("Invalid input command"), ev.Channel))
+				} else {
+					Eid = inputStringLength[2]
+					if _, err := os.Stat(fmt.Sprintf("/Users/madhav/%s", inputStringLength[2])); err != nil {
+						if os.IsNotExist(err) {
+							// file does not exist
+							f, err := os.Create(fmt.Sprintf("/Users/madhav/%s", inputStringLength[2]))
+							if err != nil {
+								fmt.Errorf("ERROR in creating a file \n")
 							}
+							w = bufio.NewWriter(f)
+							go formBotClient.sendQuestions(c)
+						} else {
+							// other error
 						}
 					}
 				}
-				if formbot {
-					n3, err := w.WriteString(fmt.Sprintf("%s\n", ev.Text))
-					if err != nil {
-						fmt.Errorf(err.Error())
-					}
-					fmt.Printf("wrote %s  %d bytes\n", ev.Text, n3)
-					w.Flush()
-					c <- questionCount
-					questionCount++
-				}
-
-			case *slack.RTMError:
-				fmt.Printf("Error: %s\n", ev.Error())
-
-			case *slack.InvalidAuthEvent:
-				fmt.Printf("Invalid credentials")
-				break Loop
-
-			default:
-				fmt.Println(msg.Type)
 			}
+
+			if formbot {
+				n3, err := w.WriteString(fmt.Sprintf("%s\n", ev.Text))
+				if err != nil {
+					fmt.Errorf(err.Error())
+				}
+				fmt.Printf("wrote %s  %d bytes\n", ev.Text, n3)
+				w.Flush()
+				c <- questionCount
+				questionCount++
+
+			}
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			break Loop
+
+		default:
+			fmt.Println(msg.Type)
 		}
+
 	}
 }
 
